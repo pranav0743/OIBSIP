0828fa45769b3d8f280e04d1426e9fc8d297f49c
pranav0743 <76099271+pranav0743@users.noreply.github.com>
2023-07-14 21:39:36 +0530

    Add files via upload

---
+++ calculator/index.js
@@ -0,0 +1,11 @@
+ function display(value){
+    document.getElementById('textarea').value+=value
+ }
+ function calculate(value) {
+    var value=document.getElementById('textarea').value
+    var answer=eval(value)
+    document.getElementById('textarea').value=answer
+ }
+ function clr() {
+    document.getElementById('textarea').value=''
+ }
